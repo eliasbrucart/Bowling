ddce8c007597cba9e14cee0e04c6dda8bde63615
Elias <eliasbrucart@hardgames.com.ar>
2021-04-11 02:20:35 -0300

    Added ball script

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float force;
+
+    public Vector3 startPos;
+    void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+        transform.position = startPos;
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void AddForce()
+    {
+        rb.AddForce(new Vector3(0.0f, 0.0f, force));
+    }
+
+    public void Right()
+    {
+        transform.position += new Vector3(1.0f, 0.0f, 0.0f);
+    }
+
+    public void Left()
+    {
+        transform.position += new Vector3(-1.0f, 0.0f, 0.0f);
+    }
+}
