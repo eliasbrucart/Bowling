12abc36973aed8e00cdadb21d4dca0a26d6e52eb
Elias <eliasbrucart@hardgames.com.ar>
2021-04-12 01:11:14 -0300

    Ball position reset ready

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,22 +6,29 @@ public class Ball : MonoBehaviour
 {
     public Rigidbody rb;
     public float force;
-
     public Vector3 startPos;
+    public float limitMove;
+
+    private bool isMoving;
     void Start()
     {
         rb.GetComponent<Rigidbody>();
         transform.position = startPos;
+        isMoving = false;
     }
 
     void Update()
     {
-
+        if(transform.position.z >= limitMove)
+        {
+            ResetPos();
+        }
     }
 
     public void AddForce()
     {
         rb.AddForce(new Vector3(0.0f, 0.0f, force));
+        isMoving = true;
     }
 
     public void Right()
@@ -32,5 +39,13 @@ public class Ball : MonoBehaviour
     public void Left()
     {
         transform.position += new Vector3(-1.0f, 0.0f, 0.0f);
+    }
+
+    public void ResetPos()
+    {
+        transform.position = startPos;
+        rb.angularVelocity = Vector3.zero;
+        rb.velocity = Vector3.zero;
+        isMoving = false;
     }
 }
