9618bee8d724cf82f9f4fdc990667c48015e0c77
Elias <eliasbrucart@hardgames.com.ar>
2021-04-11 23:08:33 -0300

    Kegels desactivate when falling

---
+++ Assets/Scripts/Kegel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kegel : MonoBehaviour
+{
+    public bool alive;
+    void Start()
+    {
+        alive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.rotation.x >= 0.5f || transform.rotation.x <= -0.5f || transform.rotation.z >= 0.5f || transform.rotation.z <= -0.5f)
+        {
+            alive = false;
+            gameObject.SetActive(false);
+        }
+    }
+}
