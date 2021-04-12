c8e21919708f5388e82dcd3f8350affd850c5fc1
Elias <eliasbrucart@hardgames.com.ar>
2021-04-11 21:47:33 -0300

    Camera now follows the ball

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Vector3 offset;
+
+    public Transform ballTransform;
+    void Start()
+    {
+        offset = new Vector3(0.0f, 3.0f, -7.0f);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = ballTransform.position + offset;
+    }
+}
