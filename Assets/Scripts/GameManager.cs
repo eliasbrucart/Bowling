768d1942fb4f2d820f94096d16b47134c95e5162
Elias <eliasbrucart@hardgames.com.ar>
2021-04-11 21:22:06 -0300

    Added GameManager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Ball ball;
+
+    public int tries;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ball.Left();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ball.Right();
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && tries <= 3)
+        {
+            ball.AddForce();
+            tries--;
+        }
+        ResetTries();
+    }
+
+    void ResetTries()
+    {
+        if(tries <= 0)
+        {
+            tries = 0;
+        }
+    }
+}
