6632cf29cc35d177242ca53d8193803e1918226e
Elias <eliasbrucart@hardgames.com.ar>
2021-04-13 18:03:55 -0300

    Menu is ready

---
+++ Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void onClickQuit()
+    {
+        Application.Quit();
+    }
+}
